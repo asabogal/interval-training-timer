0c47d92f7dd226ef0d3c1fa249247776d9890679
asabogal <asabogal.git@gmail.com>
2019-01-20 16:44:02 -0500

    fix users_controller class name; added initial functionality/added initial route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,11 @@
-class User < ApplicationController
+require 'pry'
+class UsersController < ApplicationController
+
+  def workouts
+    user = User.find(params[:id])
+    workouts = user.workouts
+    json = workouts.to_json
+    render json: json
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 
   get '/workouts', to: 'workouts#index'
   
+  get '/users/:id/workouts', to: 'users#workouts'
+
 end
