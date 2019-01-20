2d480db0fc2641fba6b49cf9b92139a9755bf49f
asabogal <asabogal.git@gmail.com>
2019-01-20 17:49:09 -0500

    add user serializer functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
-require 'pry'
 class UsersController < ApplicationController
 
   def workouts
     user = User.find(params[:id])
     workouts = user.workouts
-    json = workouts.to_json
-    render json: json
+    render json: workouts
   end
 
 end
