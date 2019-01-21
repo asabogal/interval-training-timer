21334634dbb20ec7ccb63a8c6e32d1ec6ae7e3c0
asabogal <asabogal.git@gmail.com>
2019-01-20 19:31:12 -0500

    add and tested user new and create actions; set routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,29 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+
   def workouts
-    user = User.find(params[:id])
-    workouts = user.workouts
-    render json: workouts
+    @user = User.find(params[:id])
+    @workouts = @user.workouts
+    render json: @workouts, status: 200
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password_digest)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
+  #namespace API routes....!
   get '/workouts', to: 'workouts#index'
-  
+
+  resources :users, only: [:new, :create]
   get '/users/:id/workouts', to: 'users#workouts'
 
+  
 end
