d8dcca446cbe8ccaa276124919e77789347acedc
asabogal <asabogal.git@gmail.com>
2019-01-21 18:06:15 -0500

    add and test reactstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
+import { Container, ButtonDropdown, DropdownToggle, DropdownItem, DropdownMenu } from 'reactstrap'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        TESTING 
-      
-      </div>
+     <Container>
+        <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+        <DropdownToggle caret>
+          Button Dropdown
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem header>Header</DropdownItem>
+          <DropdownItem disabled>Action</DropdownItem>
+          <DropdownItem>Another Action</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Another Action</DropdownItem>
+        </DropdownMenu>
+      </ButtonDropdown>
+     </Container> 
+     
     );
   }
 }
