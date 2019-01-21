42818a5126bd5191447d7f2ef09681f28a848a28
asabogal <asabogal.git@gmail.com>
2019-01-21 18:07:29 -0500

    set App component back to blank

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,13 @@
 import React, { Component } from 'react';
-import { Container, ButtonDropdown, DropdownToggle, DropdownItem, DropdownMenu } from 'reactstrap'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      dropdownOpen: false
-    };
-  }
-
-  toggle() {
-    this.setState({
-      dropdownOpen: !this.state.dropdownOpen
-    });
-  }
-
   render() {
     return (
-     <Container>
-        <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-        <DropdownToggle caret>
-          Button Dropdown
-        </DropdownToggle>
-        <DropdownMenu>
-          <DropdownItem header>Header</DropdownItem>
-          <DropdownItem disabled>Action</DropdownItem>
-          <DropdownItem>Another Action</DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem>Another Action</DropdownItem>
-        </DropdownMenu>
-      </ButtonDropdown>
-     </Container> 
-     
+      <div className="App">
+        <h1>APP</h1>
+      </div>
     );
   }
 }
