b4f05041d75fdeecc62afb031e1c7a201c175a41
asabogal <asabogal.git@gmail.com>
2019-01-21 19:18:10 -0500

    basic layout setup in App; to be moved

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Jumbotron, Button, Container, Col } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>APP</h1>
+        <Jumbotron>
+        <p className="lead">Hi Intensity</p>
+        <Container className="container-bg">
+          <Col>
+          <h1 className="display-1">00:30</h1>
+          </Col>
+        </Container>
+        
+       
+        <span>Time Elapsed</span> <span>Time Remaining</span> <br></br>
+        <span className="text-center">1:20</span> <span>10:20</span>
+       
+        <hr className="my-2" />
+        <div>
+          <Button className="btn-circle btn-xl" color="primary">START</Button> <br></br>
+          <br></br>
+          <Button  color="primary">RESET</Button>
+        </div>
+      </Jumbotron>
+        
       </div>
     );
   }
