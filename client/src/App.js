903a2c352d904d05b4864c4bc4088b0c45978411
asabogal <asabogal.git@gmail.com>
2019-01-21 19:39:36 -0500

    hardcode layout to be further styled and split into components

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,64 @@
 import React, { Component } from 'react';
-import { Jumbotron, Button, Container, Col } from 'reactstrap';
+import { Jumbotron, Button, Container, Col, Row } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Container>
+        <Row>
+        <Col xs="12">
         <Jumbotron>
-        <p className="lead">Hi Intensity</p>
+        <Container>
+        <div>
+        <Button>Settings</Button>
+        </div>
+        </Container>
+      
+        <br></br>
+   
         <Container className="container-bg">
           <Col>
+          Hi Intensity
           <h1 className="display-1">00:30</h1>
           </Col>
         </Container>
         
+       <Container>
+         <Row>
+         <Col>
+         <h5>01:15</h5>
+         </Col>
+         <Col>
+         <h5>10:45</h5>
+         </Col>
+         </Row>
+
+         <Row>
+         <Col>
+         Remaining
+         </Col>
+         <Col>
+         Elapsed
+         </Col>
+         </Row>
+        
+  
+       </Container>
        
-        <span>Time Elapsed</span> <span>Time Remaining</span> <br></br>
-        <span className="text-center">1:20</span> <span>10:20</span>
        
         <hr className="my-2" />
+        <br></br>
         <div>
           <Button className="btn-circle btn-xl" color="primary">START</Button> <br></br>
-          <br></br>
-          <Button  color="primary">RESET</Button>
-        </div>
-      </Jumbotron>
-        
+           <br></br>
+            <Button  color="primary">RESET</Button>
+            </div>
+          </Jumbotron>
+          </Col>
+          </Row>
+      </Container>
       </div>
     );
   }
