b090d4b2c9bc95c2710a3fd9944ccdb62d931d30
asabogal <asabogal.git@gmail.com>
2019-01-23 15:07:57 -0500

    initial Redux setup with combineReducers/rootReducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import rootReducer from './reducers/rootReducer.js'
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux'
+import logger from 'redux-logger'
+
+const store = createStore(rootReducer, applyMiddleware(logger))
+
+
+
+
+
+ReactDOM.render(
+  <Provider store={store}>
+     <App />
+  </Provider>
+ , 
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
