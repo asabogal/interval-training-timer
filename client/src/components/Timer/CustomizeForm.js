9c16d7ad4137f34e372ff477ce2bf9ad4be7a3c7
asabogal <asabogal.git@gmail.com>
2019-01-23 03:37:00 -0500

    add handle change to form to update form state

--- client/src/components/Timer/CustomizeForm.js
+++ client/src/components/Timer/CustomizeForm.js
@@ -4,31 +4,60 @@ import { Form, FormGroup, Input, Label } from 'reactstrap';
 class CustomizeForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {  };
+
+    this.state = { 
+      name: '',
+      sets: 0,
+      running_sets: 0,
+      interval: 0,
+      rest: 0,
+      running: false,
+      running_time: 0
+      };
   }
 
+  handleChange = (event) => {
+    const { name, value } = event.target
+    this.setState({
+      [name]: value
+    })
+
+    console.log(this.state)
+  }
 
   render() {
 
-    console.log(this.props)
+  
     return (
       <div>
           <Form >
             <FormGroup>
               <Label >Workout Name</Label>
-              <Input  bsSize="sm" />
+              <Input onChange={this.handleChange} 
+              name="name"
+              value={this.state.name}
+              bsSize="sm" />
             </FormGroup>
             <FormGroup>
               <Label >Number of Sets</Label>
-              <Input  bsSize="sm" />
+              <Input onChange={this.handleChange} 
+              name="sets"
+              value={this.state.sets}
+              bsSize="sm" />
             </FormGroup>
             <FormGroup>
               <Label >Interval Length</Label>
-              <Input  bsSize="sm" />
+              <Input onChange={this.handleChange} 
+              name="interval"
+              value={this.state.interval}
+              bsSize="sm" />
             </FormGroup>
             <FormGroup>
               <Label >Rest Length</Label>
-              <Input  bsSize="sm" />
+              <Input onChange={this.handleChange} 
+              name="rest"
+              value={this.state.rest}
+              bsSize="sm" />
             </FormGroup>
             <FormGroup>
               
