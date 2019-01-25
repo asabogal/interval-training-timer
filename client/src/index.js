7edb4860aff1562cb233a58505ef680898f2c12e
asabogal <asabogal.git@gmail.com>
2019-01-24 21:49:19 -0500

    apply thunk middleware

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ import { applyMiddleware, createStore } from 'redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer, applyMiddleware(logger))
+const store = createStore(rootReducer, applyMiddleware(logger, thunk))
 
 
 ReactDOM.render(
