cc61ccfe50a3910741be8548995fea3b71df758c
asabogal <asabogal.git@gmail.com>
2019-01-25 20:25:20 -0500

    change serializer to include workout data only; no user

--- app/serializers/workout_serializer.rb
+++ app/serializers/workout_serializer.rb
@@ -1,4 +1,3 @@
 class WorkoutSerializer < ActiveModel::Serializer
-  attributes :id, :name, :sets, :running_sets, :interval, :rest, :running, :running_time, :user_id
-  belongs_to :user
+  attributes :id, :name, :sets, :running_sets, :interval, :rest, :running, :running_time
 end
