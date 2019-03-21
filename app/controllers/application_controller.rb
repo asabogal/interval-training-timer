8988cfd96edf037c364454405cb9b229749b64fc
asabogal <asabogal.git@gmail.com>
2019-03-20 20:45:14 -0400

    add fallback route to index.js

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+
+  def fallback_index_html
+    render :file => 'public/index.html'
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   resources :users, only: [:create]
   get '/users/:id/workouts', to: 'users#workouts'
 
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
   
 end
