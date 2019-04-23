df1b3ae6ce1c5b781a86d02d9e0e899045c09168
asabogal <asabogal.git@gmail.com>
2019-04-23 14:30:43 -0400

    2nd attempt to fix CORS

---
+++ public/precache-manifest.e9c1465cae1373a623c7f5c7d688f52a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0afeab2b5a9333762a63",
+    "url": "/static/css/main.6cc98c28.chunk.css"
+  },
+  {
+    "revision": "0afeab2b5a9333762a63",
+    "url": "/static/js/main.0afeab2b.chunk.js"
+  },
+  {
+    "revision": "c079f3d8f138ddfd080e",
+    "url": "/static/css/1.0ea4b6bb.chunk.css"
+  },
+  {
+    "revision": "c079f3d8f138ddfd080e",
+    "url": "/static/js/1.c079f3d8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8ecbe56dff898e43751b53ef32b649d0",
+    "url": "/static/media/IntroAlert.8ecbe56d.mp3"
+  },
+  {
+    "revision": "7a55923a1bf77da585b6e8403d046964",
+    "url": "/static/media/IntervalAlert.7a55923a.mp3"
+  },
+  {
+    "revision": "3d894f4fce4809d12b95a0733fc01d9d",
+    "url": "/index.html"
+  }
+];
