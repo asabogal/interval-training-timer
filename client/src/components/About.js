8f5d821120a2def96fb70b2a27d934f62ba23dd9
asabogal <asabogal.git@gmail.com>
2019-04-24 18:21:35 -0400

    change app description

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,6 +3,7 @@ import React from 'react';
 const About = () => {
     return(
         <div>
+          <p style={{fontWeight: 'bold'}}>This app is primarily designed and styled for mobile devices</p>
           <p>High-intensity interval training (HIIT), also called high-intensity intermittent exercise (HIIE) or sprint interval training (SIT), is a form of interval training, a cardiovascular exercise strategy alternating short periods of intense anaerobic exercise with less intense recovery periods, until too exhausted to continue.  </p>
           <p>Use this timer to set your HIIT excersice and rest intervals. Typically ranging from 15 to 30 sets of 30 to 60 seeconds of excercise, followed by 10 to 20 seconds of rest.</p>
           <p>Default configurations are provided, however, you can customize your own.</p>
