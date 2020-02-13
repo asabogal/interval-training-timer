6ad08667c87ba6fba262cb0e2e43757f39346163
asabogal <asabogal.git@gmail.com>
2020-02-12 23:33:20 -0500

    add dummy wo data for testing

---
+++ node-api/dummy_data/workouts.js
@@ -0,0 +1,28 @@
+const dummyWorkouts = [
+  {
+    id: 1,
+    name: 'Low Intensity',
+    sets: 7,
+    runningSets: 0,
+    intervalTime: 35,
+    restTime: 10,
+    isRunning: false,
+    elapsedTime: 0,
+    count: 0,
+    userID: 1
+  },
+  {
+    id: 2,
+    name: 'High Intensity',
+    sets: 15,
+    runningSets: 0,
+    intervalTime: 45,
+    restTime: 10,
+    isRunning: false,
+    elapsedTime: 0,
+    count: 0,
+    userID: 2
+  }
+]
+
+module.exports = dummyWorkouts;
