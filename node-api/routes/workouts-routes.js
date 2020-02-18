9c0c0e963ea89e8c195cdc2d75831ad1a7c530ce
asabogal <asabogal.git@gmail.com>
2020-02-17 19:34:04 -0500

    setup initial workout routes

--- node-api/routes/workouts-routes.js
+++ node-api/routes/workouts-routes.js
@@ -2,13 +2,24 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET WORKOUTS')
-  res.json(
-    {
-      message: 'GET WORKOUTS SUCCESS'
-    }
-  )
+const dummyWorkouts = require('../dummy_data/workouts');
+
+router.get('/users/:id', (req, res, next) => {
+  const userID = parseInt(req.params.id);
+  const workouts = dummyWorkouts.filter(wo => wo.userID === userID);
+  console.log({workouts})
+  res.json({workouts})
+});
+
+router.get('/top_workouts', (req, res, next) => {
+  const topWorkouts = dummyWorkouts.sort((a, b) =>(a.count > b.count) ? -1 : 1).slice(0, 1)
+  res.json({topWorkouts});
+});
+
+router.get('/:id', (req, res, next) => {
+  const workoutID = parseInt(req.params.id);
+  const workout = dummyWorkouts.find(wo => wo.id === workoutID);
+  res.json({workout});
 });
 
 module.exports = router;
