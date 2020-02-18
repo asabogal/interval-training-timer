5f74e02aab1b145d6b9bbe0ac18357147360ff95
asabogal <asabogal.git@gmail.com>
2020-02-17 22:30:48 -0500

    use Controller actions

--- node-api/routes/users-routes.js
+++ node-api/routes/users-routes.js
@@ -1,19 +1,8 @@
 const express = require('express');
-
 const router = express.Router();
-const HttpError = require('../models/http-error');
 
-const dummyUsers = require('../dummy_data/users');
+const usersController = require('../controllers/users_controller');
 
-router.get('/:id', (req, res, next) => {
-  const userID = parseInt(req.params.id);
-  const user = dummyUsers.find(user => user.id === userID)
-  if (!user) {
-    return next(
-      new HttpError('User not found with that ID', 404)
-    );
-  }
-  res.json({user})
-});
+router.get('/:id', usersController.getUserById);
 
 module.exports = router;
--- node-api/routes/workouts-routes.js
+++ node-api/routes/workouts-routes.js
@@ -1,42 +1,12 @@
 const express = require('express');
-
 const router = express.Router();
-const HttpError = require('../models/http-error');
 
-const dummyWorkouts = require('../dummy_data/workouts');
+const workoutsController = require('../controllers/workouts_controller');
 
-router.get('/top_workouts', (req, res, next) => {
-  const topWorkouts = dummyWorkouts.sort((a, b) =>(a.count > b.count) ? -1 : 1).slice(0, 1)
-  if (topWorkouts.length === 0) {
-    return next(
-      new HttpError('Unexpected server error. Try again later.', 500)
-    )
-  }
-  res.json({topWorkouts});
-});
+router.get('/top_workouts', workoutsController.getTopWorkouts);
 
-router.get('/:id', (req, res, next) => {
-  const workoutID = parseInt(req.params.id);
-  const workout = dummyWorkouts.find(wo => wo.id === workoutID);
-  if (!workout) {
-    return next(
-      new HttpError('Workout not found with ID given', 404)
-    );
-  }
-  res.json({workout});
-});
+router.get('/:id', workoutsController.getWorkoutByID);
 
-router.get('/users/:id', (req, res, next) => {
-  const userID = parseInt(req.params.id);
-  const workouts = dummyWorkouts.filter(wo => wo.userID === userID);
-  console.log(workouts)
-  if (workouts.length === 0) {
-    return next(
-      new HttpError('No workouts found for given User', 404)
-    )
-  }
-  res.json({workouts})
-});
-
+router.get('/users/:id', workoutsController.getWorkoutsByUserID);
 
 module.exports = router;
