3cb29b0cbaa91458199806de64354cc950b6f206
asabogal <asabogal.git@gmail.com>
2020-02-17 21:51:39 -0500

    add error handler middleware

--- node-api/app.js
+++ node-api/app.js
@@ -12,4 +12,14 @@ app.use('/api/workouts', workoutsRoutes);
 // Users router
 app.use('/api/users', usersRoutes);
 
+// Error Handler
+app.use((error, req, res, next) => {
+  if (res.headerSent) return next(error);
+
+  res.status(error.code || 500)
+  res.json({
+    message: error.message || 'An unknown error ocurred'
+  });
+})
+
 app.listen(5000);
